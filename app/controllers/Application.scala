d9819d8756c838125320bb890126865c15b169cf
Jamie Byers <james.byers@guardian.co.uk>
2017-03-09 17:41:26 +0000

    Capi data now being pulled through.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,13 +6,22 @@ import models.Feedback
 import services.CapiQueryService.ArticleContent
 import services.{CapiQueryService,LocalDynamoService,LocalFileService}
 
+import java.net.URLDecoder
+
 
 class Application extends Controller {
   
 	def viewArticleFeedback(articleUrl: String) = Action {
 
-		val result = LocalDynamoService.getFeedback(articleUrl)
-		Ok(views.html.index(result))
+		val decodedUrl = URLDecoder.decode(articleUrl, "UTF-8")
+
+		val result = LocalDynamoService.getFeedback(decodedUrl)
+		val article = CapiQueryService.getArticleContent(decodedUrl)
+
+		article match {
+			case Some(content) => Ok(views.html.index(content, result))
+			case None => Ok("Article not found")
+		}
 
 	}
 
