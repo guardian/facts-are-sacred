ea2a596c53c495d7b32788b83fa773af631c66f2
Alex Ware <alex.ware@guardian.co.uk>
2017-03-10 11:47:59 +0000

    ALEXS STUFFFFF

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,25 +1,34 @@
 package controllers
 
-import play.api.mvc.{Controller, Action}
-
+import play.api.mvc.{Action, Controller}
 import models.Feedback
 import services.CapiQueryService.ArticleContent
-import services.{CapiQueryService,LocalDynamoService,LocalFileService}
-
+import services.{CapiQueryService, LocalDynamoService, LocalFileService}
 import java.net.URLDecoder
+
+import play.api.libs.json.Json
 
 
 class Application extends Controller {
-  
+	implicit val feedwrite = Json.writes[Feedback]
 	def viewArticleFeedback(articleUrl: String) = Action {
 
 		val decodedUrl = URLDecoder.decode(articleUrl, "UTF-8")
 
-		val result = LocalDynamoService.getFeedback(decodedUrl)
+		val results = LocalDynamoService.getFeedback(decodedUrl)
 		val article = CapiQueryService.getArticleContent(decodedUrl)
 
+		val comments = results.map{result =>
+			result match {
+				case Right(success) => Some(success)
+				case _ => None
+			}
+		}.flatten
+
+
+
 		article match {
-			case Some(content) => Ok(views.html.index(content, result))
+			case Some(content) => Ok(views.html.index(content, Json.toJson(comments)))
 			case None => Ok("Article not found")
 		}
 
